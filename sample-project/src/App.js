4a42ebb0066982acfa5f1ecbeede65c3b3859b94
ttanaka9211 <ttanaka@hatchdogs.net>
2020-06-03 09:26:37 +0000

    list3-25

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Rect from './Rect';
 //import logo from './logo.svg';
 import './App.css';
@@ -24,37 +24,73 @@ import './App.css';
 //  );
 //}
 class App extends Component {
+ input = '';
+
   msgStyle = {
-    fontSize:'24pt',
-    color:'#900',
-    margin:'20px 0px',
-    padding:'5px',
-    borderBottom:'2px solid #900'
-  }
-  btnStyle = {
     fontSize:'20pt',
-    padding:'0px 10px'
+    color:'#900', 
+    margin:'20px 0px',
+    padding:'5px'
   }
 
   constructor(props){
     super(props);
     this.state = {
-      msg:'Hello',
+      message:'type your name:'
     };
-    let timer = setInterval(()=>{
-      this.setState((state)=>({
-        msg: state.msg + '!'
-      }));
-    },10000);
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
   }
 
+  doChange(event){
+    this.input = event.target.value;
+  }
+
+  doSubmit(event){
+    this.setState({
+      message: 'Hello,' + this.input + '!!'
+    });
+    event.preventDefault();
+  }
   render(){
     return<div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
-      <p style={this.msgStyle}>{this.props.msg}</p>
+      <Message title='Children!'>
+        これはコンポーネント内のコンテンツです。
+        マルでテキストを分解し、リストにして表示します。
+        改行は必要ありません。
+      </Message>
     </div>;
   }
 }
-  
+
+class Message extends Component {
+  li = {
+    fontSize:'16pt',
+    color:'#06',
+    margin:'0px',
+    padding:'0px',
+  }
+
+  render(){
+    let content = this.props.children;
+    let arr = content.split('。');
+    let arr2 = [];
+    for(let i = 0;i < arr.length;i++){
+      if (arr[i].trim() != ''){
+        arr2.push(arr[i]);
+      }
+    }
+    let list = arr2.map((value,key)=>(
+      <li style={this.li} key={key}>{value}.</li>
+    ));
+    return(
+      <div>
+        <h2>{this.props.title}</h2>
+        <ol>{list}</ol>
+      </div>
+    );
+  }
+} 
+
 export default App;
