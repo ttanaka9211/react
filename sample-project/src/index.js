dc33e9ce271a0e24b2d96f0a523a0dc42e886472
ttanaka9211 <ttanaka@hatchdogs.net>
2020-06-05 04:54:35 +0000

    list4-11

--- sample-project/src/index.js
+++ sample-project/src/index.js
@@ -1,22 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'react-redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
-import MemoStore from './memo/Store'
-import * as serviceWorker from './serviceWorker';
+
+//ステートの値
+let state_value = {
+  counter:0,
+  message:'COUNTER'
+}
+
+//レデューサー
+function counter(state = state_value,action){
+  switch (action.type) {
+    case 'INCREMENT':
+    return {
+      counter:state.counter + 1,
+      message:'INCREMENT'
+    };
+    case 'DECREMENT':
+      return {
+        counter:state.counter - 1,
+        message:'DECREMENT'
+      };
+    case 'RESET':
+      return {
+        counter:0,
+        message:'RESET'
+      };
+    default:
+      return state;
+  }
+}
+
+//Redux persistの設定
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+//パーシスとレデューサーの作成
+const persistedRucer = persistReducer(persistConfig,counter)
+
+//ストア、パーシスターの作成
+let store = createStore(persistedRucer)
+let pstore = persistStore(store)
 
 //表示をレンダリング
 ReactDOM.render(
-  <Provider store={MemoStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>}
+        persistor={pstore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
-//ReactDOM.render(
-  //<App />,document.getElementById('root')
-//);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
